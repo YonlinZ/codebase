49f5e0d90ddb2c947fcc48180075797140082a2e
yonlin <zylin0721@foxmail.com>
2019-05-28 16:26:36 +0800

    新增算法

--- CodeBase/Algorithm/LiteAlgorithm.cs
+++ CodeBase/Algorithm/LiteAlgorithm.cs
@@ -4,11 +4,13 @@ namespace Algorithm
 {
     public static class LiteAlgorithm
     {
+        #region 分解质因数
+
         /// <summary>
         /// 分解质因数，略作优化
         /// </summary>
         /// <param name="x"></param>
-        public static void F(ulong x)
+        public static void PrimeFactorization(ulong x)
         {
             var j = Math.Sqrt(x);
             for (ulong i = 2; i <= x; i++)
@@ -16,7 +18,7 @@ namespace Algorithm
                 if (x % i == 0)
                 {
                     Console.WriteLine("{0}", i);
-                    F(x / i);
+                    PrimeFactorization(x / i);
                     break;
                 }
                 if (i > j)
@@ -30,18 +32,22 @@ namespace Algorithm
         /// 分解质因数
         /// </summary>
         /// <param name="x"></param>
-        public static void F2(ulong x)
+        public static void PrimeFactorization2(ulong x)
         {
             for (ulong i = 2; i <= x; i++)
             {
                 if (x % i == 0)
                 {
                     Console.WriteLine("{0}", i);
-                    F(x / i);
+                    PrimeFactorization2(x / i);
                     break;
                 }
             }
         }
+
+        #endregion
+
+        #region 小青蛙跳台阶
 
         /// <summary>
         /// 小青蛙跳台阶，递归算法
@@ -64,7 +70,7 @@ namespace Algorithm
         /// <returns></returns>
         public static int FrogJump2(int n)
         {
-            var result = 0; 
+            var result = 0;
             for (int i = 0; i <= n / 2; i++)
             {
                 result += C(i, n - i);
@@ -108,13 +114,17 @@ namespace Algorithm
         }
 
 
+        #endregion
+
+        #region 计算指数幂
+
         /// <summary>
-        /// 快速幂
+        /// 二分幂1
         /// </summary>
-        /// <param name="n"></param>
-        /// <param name="power"></param>
+        /// <param name="n">底数，需要与返回类型一致，否则递归处会随着迭代溢出</param>
+        /// <param name="power">指数</param>
         /// <returns></returns>
-        public static int F4(int n, int power)
+        public static long Pow1(long n, int power)
         {
             if (power == 0)
             {
@@ -128,12 +138,83 @@ namespace Algorithm
 
             if (power % 2 == 0)//n是偶数
             {
-                return F4(n * n, power / 2);
+                return Pow1(n * n, power / 2);
             }
-            else
+            return n * Pow1(n * n, power / 2);
+        }
+
+        /// <summary>
+        /// 二分幂2
+        /// </summary>
+        /// <param name="n">底数</param>
+        /// <param name="power">指数</param>
+        /// <returns></returns>
+        public static long Pow2(int n, int power)
+        {
+            if (power == 0)
             {
-                return n * F4(n * n, power / 2);
+                return 1;
             }
+
+            if (power == 1)
+            {
+                return n;
+            }
+
+            var re = Pow2(n, power >> 1);
+            re *= re;
+            if (power % 2 == 1)
+            {
+                re *= n;
+            }
+
+            return re;
+        }
+
+        /// <summary>
+        /// 快速幂
+        /// </summary>
+        /// <param name="n">底数，需要与返回类型一致，否则递归处会随着迭代溢出</param>
+        /// <param name="power">指数</param>
+        /// <returns>n^power</returns>
+        public static long Pow3(long n, int power)
+        {
+            long res = 1;
+            while (power != 0)
+            {
+                if (power % 2 == 1)//如果是奇数
+                {
+                    res *= n;
+                }
+                n *= n;
+                power >>= 1;
+            }
+
+            return res;
+        }
+        #endregion
+
+        /// <summary>
+        /// 幂模
+        /// </summary>
+        /// <param name="n">底数</param>
+        /// <param name="power">指数</param>
+        /// <param name="mod">对mod求模</param>
+        /// <returns>模</returns>
+        public static long PowMod(long n, int power, int mod)
+        {
+            long res = 1;
+            while (power != 0)
+            {
+                if (power % 2 == 1)//如果是奇数
+                {
+                    res = (res * n) % mod;
+                }
+                n = (n * n) % mod;
+                power >>= 1;
+            }
+
+            return res;
         }
     }
 }
--- CodeBase/Algorithm/Program.cs
+++ CodeBase/Algorithm/Program.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using Algorithm.LeetCodeSeries;
 
 namespace Algorithm
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             //Stopwatch sw = new Stopwatch();
             //sw.Start();
@@ -28,14 +23,17 @@ namespace Algorithm
             //Console.WriteLine();
             //Console.WriteLine($"常规：{LiteAlgorithm.FrogJump2(1)}");
 
-            var l = new LeetCode128LongestConsecutiveSequence();
-            int[] arr = {1,3,5,7,9,55,6,8,22,13,4,5,3,8,0,62,2,10};
-            var length = l.LongestConsecutive(arr);
+            //var l = new LeetCode128LongestConsecutiveSequence();
+            //int[] arr = {1,3,5,7,9,55,6,8,22,13,4,5,3,8,0,62,2,10};
+            //var length = l.LongestConsecutive(arr);
 
 
-            Console.WriteLine(length);
 
 
+
+            //Console.WriteLine(LiteAlgorithm.Pow1(2, 60));
+            //Console.WriteLine(LiteAlgorithm.Pow2(2, 60));
+            Console.WriteLine(LiteAlgorithm.PowMod(5, 21000, 17));
 
 
             Console.ReadKey();
