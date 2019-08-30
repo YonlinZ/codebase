4f24ff2344659cb3958cbc6c4448a07a60b62474
yonlin <zylin0721@foxmail.com>
2019-08-30 16:03:39 +0800

    feat: 添加 datarow 转实体

---
+++ CodeBase/CommonMethods/DataRowToEntity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace CommonMethods
+{
+    class ConvertToEntity
+    {
+        /// <summary>
+        /// DataRow 转实体类
+        /// </summary>
+        /// <typeparam name="T">实体类型</typeparam>
+        /// <param name="row"> DataRow </param>
+        /// <returns></returns>
+        public static T DataRowToEntity<T>(DataRow row) where T : class, new()
+        {
+            var type = typeof(T);
+            T instance = Activator.CreateInstance(type) as T;
+            foreach (var property in type.GetProperties())
+            {
+                if (row.Table.Columns.Contains(property.Name) && property.CanWrite)
+                {
+                    property.SetValue(instance, row[property.Name] == DBNull.Value ? null : row[property.Name], null);
+                }
+            }
+            return instance;
+        }
+    }
+}
---
+++ 测试/控件测试/UserControl1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 控件测试
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+            //Controls.Add(button1);
+        }
+    }
+}
