eb241fbd2535d557ca97738a54a4f54d8e7bb44a
yonlin zhu <zylin0721@foxmail.com>
2021-04-23 17:16:47 +0800

    .

--- CodeBase/7Zip/Program.cs
+++ CodeBase/7Zip/Program.cs
@@ -1,6 +1,7 @@
 ﻿using SevenZip;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,13 @@ namespace _7Zip
     class Program
     {
         static void Main(string[] args)
+        {
+
+            F2();
+            Console.ReadLine();
+        }
+
+        static void F1()
         {
             if (IntPtr.Size == 4)    //32位操作系统
             {
@@ -25,8 +33,26 @@ namespace _7Zip
                 tmp.ExtractArchive("temp");
             }
 
+        }
+        static void F2()
+        {
+            //if (IntPtr.Size == 4)    //32位操作系统
+            //{
+            //    SevenZipCompressor.SetLibraryPath(AppDomain.CurrentDomain.BaseDirectory + @"zip\7z.dll"); //路径指向dll文件，此处dll放在与程序相同目录，以下相同。
 
-            Console.ReadLine();
+            //}
+            //else    //64位操作系统
+            //{
+            //    SevenZipCompressor.SetLibraryPath(AppDomain.CurrentDomain.BaseDirectory + @"zip\7z64.dll");
+            //}
+            //var path = @"F:\六盘山地地形云野外科学试验特种观测仪器数据格式说明及示例\西安华腾云雷达\示例\20200925\RAW_M";
+            //DirectoryInfo folder = new DirectoryInfo(path);
+            //var files = folder.GetFiles("*.zip").Select(file=>file.FullName).ToArray();
+            //using (var tmp = new SevenZipExtractor(path))    //这里的全名称包含路径
+            //{
+            //    tmp.ExtractFiles("temp2", files);
+            //}
+
         }
     }
 }
